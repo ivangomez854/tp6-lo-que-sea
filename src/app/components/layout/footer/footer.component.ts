817f168c48d3dbc42fa6ac940d9060e57a9d70fb
Ivan Gomez <ivangomez854@gmail.com>
2023-05-04 21:58:38 -0300

    agrego numero de grupo en footer

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -6,6 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-textoFooter = 'TRABAJO PRACTICO N 6 - CÁTEDRA INGENIERIA Y CALIDAD DE SOFTWARE'
+textoFooter = 'TRABAJO PRÁCTICO N 6 - GRUPO 7 - CÁTEDRA INGENIERÍA Y CALIDAD DE SOFTWARE'
 universidad = 'UTN-FRC'
 }
